f27ca42248daf2488979b2322ce273be833afff3
Ingemar Carlsson <ingemar.m.carlsson@outlook.com>
2022-07-17 20:53:23 +0200

    factory methods

--- ShoppingCartService.Tests/ShippingCalculatorTests.cs
+++ ShoppingCartService.Tests/ShippingCalculatorTests.cs
@@ -13,14 +13,7 @@ namespace ShoppingCartService.Tests
         public void Calculate_same_city_shipping_Standard_method()
         {
             ShippingCalculator calculator = new ShippingCalculator();
-            Cart cart = new Cart()
-            {
-                CustomerId = "id1",
-                CustomerType = CustomerType.Standard,
-                Items = new List<Item>() { new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 } },
-                ShippingAddress = new Address() { City = "Dallas", Country="USA" },
-                ShippingMethod = ShippingMethod.Standard
-            };
+            Cart cart = CreateCart();
 
             var cost = calculator.CalculateShippingCost(cart);
 
@@ -31,14 +24,7 @@ namespace ShoppingCartService.Tests
         public void Calculate_other_city_shipping_Standard_method()
         {
             ShippingCalculator calculator = new ShippingCalculator();
-            Cart cart = new Cart()
-            {
-                CustomerId = "id1",
-                CustomerType = CustomerType.Standard,
-                Items = new List<Item>() { new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 } },
-                ShippingAddress = new Address() { City = "Houston", Country = "USA" },
-                ShippingMethod = ShippingMethod.Standard
-            };
+            Cart cart = CreateCartWithAddress(CreateAddress("Stockholm"));
 
             var cost = calculator.CalculateShippingCost(cart);
 
@@ -49,14 +35,7 @@ namespace ShoppingCartService.Tests
         public void Calculate_international_shipping_Standard_method()
         {
             ShippingCalculator calculator = new ShippingCalculator();
-            Cart cart = new Cart()
-            {
-                CustomerId = "id1",
-                CustomerType = CustomerType.Standard,
-                Items = new List<Item>() { new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 } },
-                ShippingAddress = new Address() { City = "Oslo", Country = "Norway" },
-                ShippingMethod = ShippingMethod.Standard
-            };
+            Cart cart = CreateCartWithAddress(CreateAddress("Stockholm", "Norway"));
 
             var cost = calculator.CalculateShippingCost(cart);
 
@@ -69,14 +48,7 @@ namespace ShoppingCartService.Tests
         public void Calculate_same_city_shipping_Expedited_method()
         {
             ShippingCalculator calculator = new ShippingCalculator();
-            Cart cart = new Cart()
-            {
-                CustomerId = "id1",
-                CustomerType = CustomerType.Standard,
-                Items = new List<Item>() { new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 } },
-                ShippingAddress = new Address() { City = "Dallas", Country = "USA" },
-                ShippingMethod = ShippingMethod.Expedited
-            };
+            var cart = CreateCartWithShippingMethod(ShippingMethod.Expedited);
 
             var cost = calculator.CalculateShippingCost(cart);
 
@@ -87,14 +59,7 @@ namespace ShoppingCartService.Tests
         public void Calculate_other_city_shipping_Expedited_method()
         {
             ShippingCalculator calculator = new ShippingCalculator();
-            Cart cart = new Cart()
-            {
-                CustomerId = "id1",
-                CustomerType = CustomerType.Standard,
-                Items = new List<Item>() { new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 } },
-                ShippingAddress = new Address() { City = "Houston", Country = "USA" },
-                ShippingMethod = ShippingMethod.Expedited
-            };
+            var cart = CreateCart(CreateAddress("Helsinki"), ShippingMethod.Expedited);
 
             var cost = calculator.CalculateShippingCost(cart);
 
@@ -105,19 +70,84 @@ namespace ShoppingCartService.Tests
         public void Calculate_international_shipping_Expedited_method()
         {
             ShippingCalculator calculator = new ShippingCalculator();
-            Cart cart = new Cart()
-            {
-                CustomerId = "id1",
-                CustomerType = CustomerType.Standard,
-                Items = new List<Item>() { new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 } },
-                ShippingAddress = new Address() { City = "Oslo", Country = "Norway" },
-                ShippingMethod = ShippingMethod.Expedited
-            };
+            var cart = CreateCart(CreateAddress("Oslo", "Norway"), ShippingMethod.Expedited);
 
             var cost = calculator.CalculateShippingCost(cart);
 
             Assert.Equal(18.0, cost);
         }
+
+        #endregion
+
+        #region builders
+        private List<Item> CreateItems(double price = 1, string name = "Trams", uint quantity = 1)
+        {
+            return new List<Item>()
+            {
+                new Item()
+                {
+                    Price = price,
+                    ProductName=name,
+                    Quantity=quantity,
+                    ProductId = $"Id{name}"
+                }
+            };
+        }
+
+        private Cart CreateCart(
+            List<Item> items,
+            Address address,
+            string customerId = "id1",
+            CustomerType type = CustomerType.Standard,
+            ShippingMethod shippingMethod = ShippingMethod.Standard)
+        {
+            return new Cart()
+            {
+                CustomerId = customerId,
+                CustomerType = CustomerType.Standard,
+                Items = new List<Item>() { new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 } },
+                ShippingAddress = new Address() { City = "Houston", Country = "USA" },
+                ShippingMethod = ShippingMethod.Standard
+            };
+        }
+
+        private Cart CreateCartWithAddress(Address address)
+        {
+            var cart = CreateCart();
+            cart.ShippingAddress = address;
+            return cart;
+        }
+
+        private Cart CreateCartWithShippingMethod(ShippingMethod method = ShippingMethod.Standard)
+        {
+            var cart = CreateCart();
+            cart.ShippingMethod = method;
+            return cart;
+        }
+
+        private Cart CreateCart(Address address = null, ShippingMethod method = ShippingMethod.Standard) =>
+            new Cart()
+            {
+                CustomerId = "id1",
+                CustomerType = CustomerType.Standard,
+                Items = CreateItems(),
+                ShippingAddress = address ??= CreateAddress(),
+                ShippingMethod = method
+            };
+
+        private List<Item> CreateItems() =>
+            new List<Item>()
+            {
+                new Item() { Price = 1, ProductName = "Bengt", Quantity = 1 }
+            };
+
+        private Address CreateAddress(string city = "Dallas", string country = "USA", string street = "1234 left lane.") =>
+            new Address()
+            {
+                City = city,
+                Country = country,
+                Street = street
+            };
 
         #endregion
     }
